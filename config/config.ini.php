abd4991df7a3fe27a009dde5ed35e03d0f7672c9
Alexandre Baviello <alexandre.b@stride-up.fr>
2025-11-13 12:07:46 +0100

    fix: valid RSS-Bridge config.ini.php

--- config/config.ini.php
+++ config/config.ini.php
@@ -1,12 +1,21 @@
 <?php
-return [
-  'cache' => 'FileCache',
-  'cache_timeout' => 3600, // 1h
-  'debug' => false,
-  'whitelist' => [
-    'mode' => 'whitelist',
-    'bridges' => [
-      'LinkedInPublicBridge', // autorise ton bridge
-    ]
-  ],
-];
+
+// Configuration minimale RSS-Bridge
+return array(
+    // Type de cache (fichier sur disque)
+    'cache' => 'FileCache',
+
+    // Durée de cache en secondes (ici 1h)
+    'cache_timeout' => 3600,
+
+    // Mode debug (false en prod)
+    'debug' => false,
+
+    // Whitelist : n’autoriser que certains bridges
+    'whitelist' => array(
+        'mode' => 'whitelist',
+        'bridges' => array(
+            'LinkedInPublicBridge',
+        ),
+    ),
+);
